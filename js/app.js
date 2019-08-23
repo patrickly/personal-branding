d0d4e941b0ac5989e4a5a7770347a3c01c65b28f
Patrick Ly <patrickkimly@gmail.com>
2019-08-22 18:46:43 -0700

    Javascript for the mobile menu

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var menuBtn = document.getElementsByClassName('menu-btn');
+var mobileMenu = document.getElementsByClassName('mobile-menu');
+
+var clickedBtn = function() {
+    mobileMenu[0].classList.toggle('active');
+}
+
+menuBtn[0].addEventListener('click', clickedBtn);
+console.log('menuBtn:', menuBtn[0]);
